1cbba42dae30ed85cb3d75dcc6e5da9338bb45df
Genmar Dadivo <31000214+genmar-dadivo@users.noreply.github.com>
2022-10-03 09:50:02 +0800

    Update script.js

--- projects/INV/assets/js/script.js
+++ projects/INV/assets/js/script.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
-    $('.loading-screen').fadeOut();
+    function onImagesLoaded(container, event) {
+        var images = container.getElementsByTagName("img");
+        var loaded = images.length;
+        for (var i = 0; i < images.length; i++) {
+            if (images[i].complete) {
+                loaded--;
+            }
+            else {
+                images[i].addEventListener("load", function() {
+                    loaded--;
+                    if (loaded == 0) {
+                        event();
+                    }
+                });
+            }
+            if (loaded == 0) {
+                event();
+            }
+        }
+    }
+    
+    var container = document.getElementById("container");
+    
+    onImagesLoaded(container, function() {
+        //alert("All the images have loaded");
+        $('.loading-screen').fadeOut();
+    });
+
     $(document).on("click", '[data-toggle="lightbox"]', function(event) {
         event.preventDefault();
         $(this).ekkoLightbox();
