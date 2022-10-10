cb7a289aff289dce4597d40674fd2619e59ed548
Genmar Dadivo <31000214+genmar-dadivo@users.noreply.github.com>
2022-10-10 09:10:26 +0800

    Update script.js

--- projects/INV/assets/js/script.js
+++ projects/INV/assets/js/script.js
@@ -1,31 +1,16 @@
 $(document).ready(function() {
-    function onImagesLoaded(container, event) {
-        var images = container.getElementsByTagName("img");
-        var loaded = images.length;
-        for (var i = 0; i < images.length; i++) {
-            if (images[i].complete) {
-                loaded--;
-            }
-            else {
-                images[i].addEventListener("load", function() {
-                    loaded--;
-                    if (loaded == 0) {
-                        event();
-                    }
-                });
-            }
-            if (loaded == 0) {
-                event();
-            }
-        }
-    }
+    $("<img/>")
+    .on('load', function() { $('.loading-screen').fadeOut(); })
+    .on('error', function() { console.log("error loading image"); })
+    .attr("src", $(originalImage).attr("src"))
+;
     
-    var container = document.getElementById("container");
+    // var container = document.getElementById("container");
     
-    onImagesLoaded(container, function() {
-        //alert("All the images have loaded");
-        $('.loading-screen').fadeOut();
-    });
+    // onImagesLoaded(container, function() {
+    //     //alert("All the images have loaded");
+    //     $('.loading-screen').fadeOut();
+    // });
 
     $(document).on("click", '[data-toggle="lightbox"]', function(event) {
         event.preventDefault();
